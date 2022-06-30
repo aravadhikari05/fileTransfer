f77fac90a462dc2ddbf80cf62418c4411eea07b8
Arav Adhikari <arav@Aravs-MacBook-Air.local>
2022-06-29 19:59:41 -0700

    create classes for mvc

---
+++ src/Controller.java
@@ -0,0 +1,54 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements Observer, Runnable{
+
+    private GUI gui;
+    private FileSender client;
+    private FileReceiver server;
+
+    private String ip;
+    private String filePath;
+    private int port;
+
+    public Controller (GUI gui, FileSender client, FileReceiver server, int port) {
+        this.gui = gui;
+        this.client = client;
+        this.server = server;
+
+        this.ip = "";
+        this.filePath = "";
+        this.port = port;
+
+        gui.addObserver(this);
+    }
+
+    public void startThreads() {
+        Thread t1 = new Thread(server);
+        Thread t2 = new Thread(this);
+
+        t1.start();
+        t2.start();
+    }
+
+    @Override
+    public void selectedIPWasChanged(GUI panel) {
+        if(panel.getSelectedIP() == null) ip = panel.getSelectedIP();
+    }
+
+    @Override
+    public void selectedFileWasChanged(GUI panel) {
+        if(panel.getSelectedFilePath() != null) filePath = panel.getSelectedFilePath();
+    }
+
+    @Override
+    public void sendButtonWasPressed(GUI gui) {
+        System.out.println(filePath + ip);
+        if(filePath != "" || ip != "") client.sendFile(filePath, ip);
+    }
+
+    @Override
+    public void run() {
+        gui.updateDropdown(StreamTools.pingForConnections(port));
+    }
+}
---
+++ src/Observer.java
@@ -0,0 +1,5 @@
+public interface Observer {
+    public void selectedIPWasChanged(GUI gui);
+    public void selectedFileWasChanged(GUI gui);
+    public void sendButtonWasPressed(GUI gui);
+}
