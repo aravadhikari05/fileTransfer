da75bc5dffcfed275aecb574ae6f891188773488
Arav Adhikari <arav@Aravs-MacBook-Air.local>
2022-06-29 20:02:29 -0700

    create threads and instantiate classes

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        createFrame();
+        int port = 55219;
+        //temp-----
+        String file = "src/newfile.txt";
+        //---------
+        GUI gui = new GUI();
+        createFrame(gui);
+        FileSender client = new FileSender(port);
+        FileReceiver server = new FileReceiver(file, port);
+        Controller controller = new Controller(gui, client, server, port);
+
+        controller.startThreads();
+
     }
 
-    public static void createFrame() {
+    public static void createFrame(GUI panel) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.out.println(e);
+        }
+
         JFrame frame = new JFrame("File Transfer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new GUI());
         frame.setResizable(false);
+        frame.add(panel);
+
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
 }
